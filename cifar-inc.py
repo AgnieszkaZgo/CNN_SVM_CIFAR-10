b1fda2bafdd04e7317e21bd44b82fd17846294e7
AgnieszkaZgo <agnieszka.zgorzelska@gmail.com>
2019-02-03 19:17:18 +0100

    ...

--- cifar-inc.py
+++ cifar-inc.py
@@ -5,6 +5,8 @@ from keras.applications.inception_v3 import InceptionV3
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 from PIL import Image
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -23,15 +25,19 @@ model_inc = InceptionV3(weights='imagenet', include_top=False, input_shape=(75, 
 codes_train = model_inc.predict(X_train).reshape(x_train.shape[0], -1)
 codes_test = model_inc.predict(X_test).reshape(x_test.shape[0], -1)
 
-scaler = StandardScaler()
-scaler.fit(codes_train)
-codes_train = scaler.transform(codes_train)
-svc = svm.LinearSVC(C=10, dual=False, penalty='l1', max_iter=5000)
-svc.fit(codes_train, y_train.flatten())
 
-codes_test = scaler.transform(codes_test)
-pred = svc.predict(codes_test)
-pred_2 = svc.predict(codes_train)
+pipe = Pipeline([('standarize', StandardScaler()), ('model', svm.SVC(dual=False))])
+
+param_grid = {'model__penalty': ['l1', 'l2'],
+              'model__C': [1e-4, 1e-3, 0.01, 0.1, 1]}
+
+gs = GridSearchCV(pipe, param_grid, cv=3, n_jobs=-1, verbose=1, return_train_score=True)
+gs.fit(codes_train, y_train.flatten())
+print('Best parameters:', gs.best_params_)
+
+best_model = gs.best_estimator_
+pred = best_model.predict(codes_test)
+pred_2 = best_model.predict(codes_train)
 
 print('Accuracy on test set:', accuracy_score(pred, y_test.flatten()))
 print('Accuracy on train set:', accuracy_score(pred_2, y_train.flatten()))
