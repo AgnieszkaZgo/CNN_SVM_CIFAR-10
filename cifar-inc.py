9df39bb66aa97b5a61e811c85d0e614bce1f4e2b
AgnieszkaZgo <agnieszka.zgorzelska@gmail.com>
2019-02-04 08:11:45 +0100

    ...

--- cifar-inc.py
+++ cifar-inc.py
@@ -26,10 +26,12 @@ codes_train = model_inc.predict(X_train).reshape(x_train.shape[0], -1)
 codes_test = model_inc.predict(X_test).reshape(x_test.shape[0], -1)
 
 
-pipe = Pipeline([('standarize', StandardScaler()), ('model', svm.LinearSVC(dual=False, max_iter=5000))])
+pipe = Pipeline([('standarize', StandardScaler()), ('model', svm.LinearSVC(dual=False, max_iter=10000))])
 
 param_grid = {'model__penalty': ['l1', 'l2'],
-              'model__C': [1e-4, 1e-3, 0.01, 0.1, 1]}
+              'model__C': [0.005, 0.008, 0.01, 0.02, 0.05],
+              'model_loss': ['hinge', 'squared_hinge'],
+              'model_tol': [1e-5, 1e-4, 1e-3]}
 
 gs = GridSearchCV(pipe, param_grid, cv=3, n_jobs=-1, verbose=1, return_train_score=True)
 gs.fit(codes_train, y_train.flatten())
@@ -39,23 +41,23 @@ best_model_1 = gs.best_estimator_
 pred_1 = best_model_1.predict(codes_test)
 pred_2_1 = best_model_1.predict(codes_train)
 
-param_grid = {'penalty': ['l1', 'l2'],
-              'C': [1e-4, 1e-3, 0.01, 0.1, 1]}
-
-gs = GridSearchCV(svm.LinearSVC(dual=False, max_iter=5000), param_grid,
-                  cv=3, n_jobs=-1, verbose=10, return_train_score=True)
-gs.fit(codes_train, y_train.flatten())
-best_2 = gs.best_params_
-
-best_model_2 = gs.best_estimator_
-pred_2 = best_model_2.predict(codes_test)
-pred_2_2 = best_model_2.predict(codes_train)
+# param_grid = {'penalty': ['l1', 'l2'],
+#               'C': [1e-4, 1e-3, 0.01, 0.1, 1]}
+#
+# gs = GridSearchCV(svm.LinearSVC(dual=False, max_iter=5000), param_grid,
+#                   cv=3, n_jobs=-1, verbose=10, return_train_score=True)
+# gs.fit(codes_train, y_train.flatten())
+# best_2 = gs.best_params_
+#
+# best_model_2 = gs.best_estimator_
+# pred_2 = best_model_2.predict(codes_test)
+# pred_2_2 = best_model_2.predict(codes_train)
 
 print('Scalar Accuracy on test set:', accuracy_score(pred_1, y_test.flatten()))
 print('Scalar Accuracy on train set:', accuracy_score(pred_2_1, y_train.flatten()))
 print('Best parameters:', best_1)
 print('-----------------')
-print('Accuracy on test set:', accuracy_score(pred_2, y_test.flatten()))
-print('Accuracy on train set:', accuracy_score(pred_2_2, y_train.flatten()))
-print('Best parameters:', best_2)
+# print('Accuracy on test set:', accuracy_score(pred_2, y_test.flatten()))
+# print('Accuracy on train set:', accuracy_score(pred_2_2, y_train.flatten()))
+# print('Best parameters:', best_2)
 
