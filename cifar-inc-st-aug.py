fdb672dc6f359d0c019ae2f22370d0a564f36c27
AgnieszkaZgo <agnieszka.zgorzelska@gmail.com>
2019-02-05 16:16:50 +0100

    ...

--- cifar-inc-st-aug.py
+++ cifar-inc-st-aug.py
@@ -2,6 +2,8 @@ from keras.datasets import cifar10
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 import pickle
 import numpy as np
 
@@ -19,20 +21,23 @@ codes_train = np.vstack([codes_train, codes_train_flip])
 (_, y_train), (_, y_test) = cifar10.load_data()
 y_train = np.vstack([y_train, y_train])
 
-param_grid = [{'kernel': ['linear'], 'C': [0.01, 0.01, 0.1, 1]},
-              {'kernel': ['rbf'], 'C': [0.01, 0.01, 0.1, 1], 'gamma':[1e-5, 1e-4, 1e-3, 1e-2]}]
+param_grid = [{'model__kernel': ['linear'], 'model__C': [0.01, 0.01, 0.1, 1]},
+              {'model__kernel': ['rbf'], 'model__C': [1e-4, 0.001, 10, 100], 'model__gamma':[1e-6, 1e-4, 0.1, 'scale']}]
 
-gs = GridSearchCV(svm.SVC(), param_grid, cv=3, n_jobs=-1, verbose=10, return_train_score=True)
+pipe = Pipeline([('scaler', StandardScaler()), ('model', svm.SVC())])
+
+gs = GridSearchCV(pipe, param_grid, cv=3, n_jobs=-1, verbose=10, return_train_score=True)
 gs.fit(codes_train, y_train.flatten())
 print('Best parameters:', gs.best_params_)
 
-best_model = gs.best_estimator_
+best_model_st = gs.best_estimator_
 
-with open('/home/agnieszka/best_model_aug.pkl', 'wb') as model:
-    pickle.dump(best_model, model)
+with open('/home/agnieszka/best_model_st_aug.pkl', 'wb') as model:
+    pickle.dump(best_model_st, model)
 
-pred_1 = best_model.predict(codes_test)
-pred_2 = best_model.predict(codes_train)
+pred_1 = best_model_st.predict(codes_test)
+pred_2 = best_model_st.predict(codes_train)
 
 print('Accuracy on test set:', accuracy_score(pred_1, y_test.flatten()))
 print('Accuracy on train set:', accuracy_score(pred_2, y_train.flatten()))
+
